658a10a32f81d82daf76d88f1e5648a24ed34971
Sanat Sinha <itssanat196@gmail.com>
2025-07-13 16:50:29 +0530

    Added sendMessage controller with Cloudinary upload for image support

--- Backend/src/controllers/message.controller.js
+++ Backend/src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const getUserForSidebar = async(req, res) => {
     try{
@@ -28,4 +29,35 @@ export const getMessage = async(req, res) => {
         console.log("Error in getMessage controller: ", error.message);
         res.status(500).json({error: "Internal server error"});
     }
-}
+};
+
+export const sendMessage = async(req, res) => {
+    try {
+        const {text, image} = req.body;
+        const {id: receiverId} = req.params;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if(image) {
+            //Upload base64 image to cloudinary
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            tect,
+            image: imageUrl,
+        });
+
+        await newMessage.save();
+
+        res.status(201).json(newMessage);
+
+        // Todo realtime functionality goes here => socket.io
+    } catch (error) {
+        console.log("Error in getMessage controller: ", error.message);
+        res.status(500).json({error: "Internal server error"});
+    }
+};
