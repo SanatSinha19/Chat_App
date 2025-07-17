5001752ca084731063939eae037d29e07aad779f
Sanat Sinha <itssanat196@gmail.com>
2025-07-17 18:59:13 +0530

    Set up basic routing, authentication check with Zustand store, and conditional loading spinner

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,6 +4,7 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config()
 
@@ -15,6 +16,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRoutes)
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
+  
 
 app.get("/", (req, res) => {
   res.send("Hello from backend");
