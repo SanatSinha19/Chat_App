d053b4c808fe2819364aec2d4740f71c739e9629
Sanat Sinha <itssanat196@gmail.com>
2025-07-23 14:40:49 +0530

    Just improved somethings

--- Backend/src/controllers/message.controller.js
+++ Backend/src/controllers/message.controller.js
@@ -15,6 +15,7 @@ export const getUsersForSidebar = async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
 export const getMessages = async (req, res) => {
   try {
     const { id: userToChatId } = req.params;
